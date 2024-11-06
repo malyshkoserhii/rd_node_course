e9410633c5b260ae958af1a29a685b3ac5208b76
Serhii Malyshko <serhiimalyshko@Serhiis-MacBook-Pro.local>
2024-11-06 11:27:31 +0200

    Completed hw-07

---
+++ hw-07/task.sql
@@ -0,0 +1,25 @@
+CREATE TABLE students (
+
+   id SERIAL PRIMARY KEY,
+
+   name VARCHAR(100)
+
+);
+
+CREATE OR REPLACE PROCEDURE add_student(student_name VARCHAR)
+
+LANGUAGE plpgsql
+
+AS $$
+
+BEGIN
+
+   INSERT INTO students (name) VALUES (student_name);
+
+END;
+
+$$;
+
+
+
+CALL add_student('John Doe');
